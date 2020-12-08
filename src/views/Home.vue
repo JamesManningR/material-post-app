858ebff581f28242637c1ee31f700077f9a7647c
James Manning <jamesmanningr98@gmail.com>
2020-12-08 11:55:46 +0000

    Added post card

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,22 +4,14 @@
       <h1 class="display-3 mb-2 font-weight-bold">Posts</h1>
       <section>
         <h2>Create a post</h2>
-        <post-edit-form isNew v-model="post">
+        <post-edit-form isNew>
           <v-btn color="primary" slot="submit" type="submit"> Create </v-btn>
         </post-edit-form>
       </section>
       <section>
         <h2>Here is a list of posts</h2>
-        <ul>
-          <article>
-            <h3>Post title</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus
-              saepe laudantium distinctio. Quo fugit dolores, animi asperiores
-              modi tenetur consequatur doloremque dolor doloribus, quis eaque,
-              officia ad esse quia inventore?
-            </p>
-          </article>
+        <ul v-for="(post, key) in orderedPosts" :key="key">
+          <post-card :postId="post.id" :post="post"></post-card>
         </ul>
       </section>
     </v-container>
@@ -27,20 +19,22 @@
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 import PostEditForm from "@/components/posts/PostEditForm";
+import PostCard from "@/components/posts/PostCard";
 
 export default {
   name: "Home",
   components: {
-    PostEditForm
+    PostEditForm,
+    PostCard
   },
-  data() {
-    return {
-      post: {
-        title: "",
-        body: ""
-      }
-    };
+  computed: {
+    ...mapGetters("post", ["orderedPosts"])
+  },
+  beforeCreate() {
+    this.$store.dispatch("post/fetchPosts");
   }
 };
 </script>
